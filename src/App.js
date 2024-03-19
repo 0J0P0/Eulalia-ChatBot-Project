38fe49eba1da868aff032c8af911c3ded2e0a379
JP <jpniznhy@hotmail.com>
2024-03-19 09:26:31 +0100

    quisom size complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header.js'
 import SideBar from './components/sidebar.js';
 import Quisom from './pages/Quisom.js';
+import Chat from './pages/Chat.js';
+import Ajuda from './pages/Ajuda.js';
 
 function App() {
   const [sidebarOpen, setSideBarOpen] = useState(false);
@@ -15,10 +17,15 @@ function App() {
   
   return (
     <Router>
+      {/* <Home /> */}
       <Header onClick={handleViewSidebar} />
       <SideBar isOpen={sidebarOpen}/>
+      <p>
+      </p>
         <Routes>
           <Route path='/quisom' element={<Quisom/>} />
+          <Route path='/chat' element={<Chat/>} />
+          <Route path='/ajuda' element={<Ajuda/>} />
         </Routes>
     </Router>
 );
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -10,6 +10,7 @@ const SideBar = props => {
         <div className='sidebar_links'>
             <ul>
                 <li><Link to='/'>Inici</Link></li>
+                <li><Link to='/chat'>Eulàlia</Link></li>
                 <li><Link to='/quisom'>Qui som?</Link></li>
                 <li><Link to='/ajuda'>Ajuda</Link></li>
             </ul>
