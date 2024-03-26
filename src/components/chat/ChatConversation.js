fe5681b41825bc5b38f8794810e9fdb944da30b2
JP <jpniznhy@hotmail.com>
2024-03-26 20:09:12 +0100

    custom message list

--- src/components/chat/ChatConversation.js
+++ src/components/chat/ChatConversation.js
@@ -1,38 +1,52 @@
 import React from 'react';
-import { MessageList, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 import eulalia_message_logo from '../../img/eulalia_message_logo.svg';
 import user_message_logo from '../../img/user_message_logo.svg';
+
+import { TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+// import MessageList from './MessageList.js';
 
 import '../../styles/chat_conversation.css';
 
 
 function ChatConversation({ messages, isTyping }) {
+  // Ref for scrolling to bottom
+  const messagesEndRef = React.useRef(null);
+
+  // Function to scroll to bottom
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  React.useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <MessageList 
-      scrollBehavior='smooth' 
-      typingIndicator={isTyping ? <TypingIndicator content='Eulàlia is typing' /> : null}
-    >
+    <div className='message_list_container'>
       {messages.map((message, index) => (
-      <div key={index} className='message_container'>
-        <img
-          src={message.sender === 'Eulàlia' ? eulalia_message_logo : user_message_logo}
-          alt='Message Logo'
-          className='message_logo' />
-        <div>
-          {message.sender === 'Eulàlia' ? (
-            <div className='eulalia_message'>
+        <div key={index} className='message_container'>
+          <img
+            src={message.sender === 'Eulàlia' ? eulalia_message_logo : user_message_logo}
+            alt='Message Logo'
+            className='message_logo' />
+          <div>
+            {message.sender === 'Eulàlia' ? (
+              <div className='eulalia_message'>
                 {message.message}
-            </div>
-          ) : (
-            <div className='user_message'>
-              {message.message}
-            </div>
-          )}
+              </div>
+            ) : (
+              <div className='user_message'>
+                {message.message}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
       ))}
-    </MessageList>
+      {isTyping && <TypingIndicator content='Eulàlia is typing' />}
+      <div ref={messagesEndRef} /> {/* Ref for scrolling to bottom */}
+    </div>
   )
 }
 
