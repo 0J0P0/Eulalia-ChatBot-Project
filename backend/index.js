75de3d4336ab21cecf67fb1974e35de8f10e8939
JP <jpniznhy@hotmail.com>
2024-04-16 10:47:48 +0200

    Fix CORS issue in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ const port = 3000
 const users_model = require('./UserConnection')
 
 app.use(express.json())
+
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
-
 
 app.post('/', (req, res) => {
   users_model.createUser(req.body)
