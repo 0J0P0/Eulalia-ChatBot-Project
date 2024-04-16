64b1c5979b3cce867fbf0e30dafabf1cae170ed2
JP <jpniznhy@hotmail.com>
2024-04-16 10:31:33 +0200

    test commit

---
+++ eulalia-master/src/components/MainLogo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import '../styles/main_logo.css';
+
+import main_eulalia_logo from '../img/main_eulalia_logo.png';
+
+function MainLogo() {
+  return (
+    <div className='main_logo_container'>
+      <img className='logo_img' src={main_eulalia_logo} alt='Eulalia MainLogo'/>
+      <h1>
+        <span class="black-text">Eulàl</span>
+        <span class="blue-text">ia</span>
+      </h1>
+      <div className='page_subtitle'>
+        <h2> FEM BARCELONA </h2>
+      </div>
+    </div>
+  );
+};
+
+export default MainLogo;
---
+++ eulalia-master/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../styles/footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>2023 by Eulàlia. Proudly created with React</p>
+    </div>
+  )
+}
+
+export default Footer
