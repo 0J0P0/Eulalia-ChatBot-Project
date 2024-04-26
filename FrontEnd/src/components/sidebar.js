16274118289b12ff30ca656b7db8f894a4d8930d
JP <jpniznhy@hotmail.com>
2024-04-26 20:28:45 +0200

    add logout icon

--- FrontEnd/src/components/sidebar.js
+++ FrontEnd/src/components/sidebar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import '../styles/sidebar.css';
 
+import logoutImg from '../img/logout.svg';
+
 const SideBar = props => {
     const sidebarClass = props.isOpen ? "sidebar open" : "sidebar";
 
@@ -14,19 +16,24 @@ const SideBar = props => {
     };
 
     return (
-      <div className={sidebarClass}>
-        <div className='sidebar_links'>
-            <ul>
-                <li><Link to='/'>Inici</Link></li>
-                <li><Link to='/bot'>Eulàlia</Link></li>
-                <li><Link to='/quisom'>Qui som?</Link></li>
-                <li><Link to='/ajuda'>Ajuda</Link></li>
-                {props.authenticated && (
-                    <li><button className='logout_button' onClick={handleLogout}>Tanca Sessió</button></li>
-                )}
-            </ul>
+        <div className={sidebarClass}>
+            <div className='sidebar_links'>
+                <ul>
+                    <li><Link to='/'>Inici</Link></li>
+                    <li><Link to='/bot'>Eulàlia</Link></li>
+                    <li><Link to='/quisom'>Qui som?</Link></li>
+                    <li><Link to='/ajuda'>Ajuda</Link></li>
+                    {props.authenticated && (
+                        <li className="logout_container">
+                            <button className='logout_button' onClick={handleLogout}>
+                                <img src={logoutImg} alt="logout" className="logout_icon" onClick={handleLogout} />
+                                Tanca Sessió
+                            </button>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
-      </div>
     );
 }
 
