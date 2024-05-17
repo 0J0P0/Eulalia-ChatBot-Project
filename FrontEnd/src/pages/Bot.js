75cd70051053b08be2f845a221ed05830274c758
JP <jpniznhy@hotmail.com>
2024-05-17 09:51:35 +0200

    conversation list

--- FrontEnd/src/pages/Bot.js
+++ FrontEnd/src/pages/Bot.js
@@ -13,6 +13,7 @@ import new_chat_icon from '../img/new_chat.svg';
 function Bot() {
   const [messages, setMessages] = useState([]);
   const [isTyping, setIsTyping] = useState(false);
+  const [conversationIds, setConversationIds] = useState([]);
 
   useEffect(() => {
     const storedMessages = localStorage.getItem('chatMessages');
@@ -53,37 +54,33 @@ function Bot() {
     .catch(error => console.error('Error:', error));
   }
 
-  // const handleNewChat = () => {
-  //   setMessages([]);
-  //   localStorage.removeItem('chatMessages');
-  // };
-
   const handleNewChat = () => {
     // Reset chat history
     setMessages([]);
     localStorage.removeItem('chatMessages');
 
-    // // Send request to backend: refresh history bar
-    // fetch('/api/refresh_history', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({})
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //     // Handle response from backend if needed
-    // })
-    // .catch(error => console.error('Error:', error));
-};
+    // Send request to backend: refresh history bar
+    fetch('/api/refresh_history', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Conversation ids:', data);
+        setConversationIds(data);
+    })
+    .catch(error => console.error('Error:', error));
+  };
 
   return (
     <div>
       <Logo subtitle='' />
       <div className='chat_container'>
         <div className='left_chat_col'>
-          <ChatHistory />
+          <ChatHistory conversationIds={conversationIds}/>
           <button onClick={handleNewChat} className='new_chat_button'>
             <img
               src={new_chat_icon}
