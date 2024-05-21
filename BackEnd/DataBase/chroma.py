ad59c556e0c0fbca2f51dcd4c9f857bc8ea6f88f
Guillemdecabanyesupc <j.blanco.arnaus@gmail.com>
2024-05-21 21:21:37 +0200

    ChromaDB update

--- BackEnd/DataBase/chroma.py
+++ BackEnd/DataBase/chroma.py
@@ -1,8 +1,12 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 import nltk
 import re
-import re
 import os
+import ast
 import json
+import dotenv
 import openai
 import chromadb
 import pandas as pd
@@ -11,23 +15,53 @@ from unidecode import unidecode
 from nltk.corpus import stopwords
 from chromadb.utils import embedding_functions
 
-os.environ["OPENAI_API_KEY"]="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"
+dotenv.load_dotenv()
+os.environ["OPENAI_API_KEY"] = str(os.getenv("API_KEY"))
 nltk.download('stopwords')
 
+
 def get_data():
-    df = pd.read_json('/home/user/eulalia/Data/Dades_solr_pro/yol_definicio_indicadors_collection_k8s_20240215.json', lines=True)
+    """
+    Extract data from the JSON file.
+
+    Returns
+    -------
+    dt : DataFrame
+        DataFrame containing all the data regarding the DataBase.
+    """
+
+    df = pd.read_json('/dades/eulalia/Data/Dades_solr_pro/yol_definicio_indicadors_collection_k8s_20240215.json', lines=True)
     dd = df[df['api_origen']=='estadistiques'].dropna(axis=1, how='all')
 
     dt = dd[['descripcio.ca', 'id_indicador', 'tags.ca', 'fet.ca', 'indicador.ca', 'tipus_territori.ca', 'unitat.ca', 'unitat_mesura.ca',  'valors_territori', 'llistat_dimensions.ca', 'notes_indicador.ca', 'valors_dimensions.ca']]
     dt['tipus_territori.ca'] = dt['tipus_territori.ca'].astype(str)
 
     dt['id_indicador'] = dt['id_indicador'].apply(lambda x: 'a' + x if x[0].isdigit() else x)
-
-    print('get_data')
     return dt
 
 
 def get_territory_values(dt):
+    """
+    Extract territorial values from the data.
+
+    Parameters
+    ----------
+    dt : DataFrame
+        DataFrame containing all the data regarding the DataBase.
+
+    Returns
+    -------
+    Municipi : List
+        List containing all values corresponding to a Municipality.
+    AreaMetropolitana : List
+        List containing all values corresponding to a Metropolitan Area.
+    ComunitatAutonoma : List 
+        List containing all values corresponding to a Autonomous Community.
+    Districte : List
+        List containing all values corresponding to a District.
+    Barri : List
+        List containing all values corresponding to a Neighborhood.
+    """
 
     Municipi = dt[dt['tipus_territori.ca'] == "['Municipi']"]['valors_territori'].value_counts().keys().tolist()[0]
     AreaMetropolitana = dt[dt['tipus_territori.ca'] == "['Àrea Metropolitana']"]['valors_territori'].value_counts().keys().tolist()[0]
@@ -65,37 +99,53 @@ def get_territory_values(dt):
     flat_list = [string for sublist in new for string in sublist]
     Barri = list(set(flat_list))
 
-    print('get_territory_values')
     return Municipi, AreaMetropolitana, ComunitatAutonoma, Districte, Barri
 
 
 def encode(string):
+    """
+    Write strings in plain text.
+
+    Parameters
+    ----------
+    string : str
+        Any string value.
+
+    Returns
+    -------
+    string : str
+        String in plain text.
+    """
     return re.sub(r'\W+', ' ', unidecode(string).lower()) 
-
-def clean_text(text, chars_to_remove):
-    cleaned_text = ''.join([c for c in text if c not in chars_to_remove])
-    cleaned_text = re.sub(r'\bnan\b', '', cleaned_text, flags=re.IGNORECASE)
-    return cleaned_text
-
-
-def generar_text(row):
-    descripcio_ca = row['descripcio.ca'] 
-    indicador_ca = row['indicador.ca'] 
-    fet_ca = row['fet.ca']  
-    unitat_mesura_ca = row['unitat_mesura.ca']  
-    tipus_territori_ca = row['tipus_territori.ca']  
-    llistat_dimensions_ca = row['llistat_dimensions.ca']  
-    tags_ca = row['tags.ca'] 
-
-    text = f"La taula conté informació de {descripcio_ca} amb l'indicador {indicador_ca}. Es mostra el fet {fet_ca} mesurat en {unitat_mesura_ca},"
-    text_2 = f" en funció d'un conjunt de dimensions: {tipus_territori_ca}, {llistat_dimensions_ca}. La informació està relacionada amb les temàtiques: {tags_ca}."
-    chars_to_remove = "!@#^&*[]'"
-    text_2 = clean_text(text_2, chars_to_remove)
-
-    return text+text_2 #encode(text+text_2)
 
 
 def fuzzy_search_and_modify(word, stop_words_catalan, Barri, Districte, Municipi, ComunitatAutonoma):
+    """
+    Fuzzy search for the 'territori' values.
+
+    Parameters
+    ----------
+    word : str
+        Any string from a sequence (word from the query).
+    stop_words_catalan : List
+        List containing all catalan stopwords.
+    Municipi : List
+        List containing all values corresponding to a Municipality.
+    AreaMetropolitana : List
+        List containing all values corresponding to a Metropolitan Area.
+    ComunitatAutonoma : List 
+        List containing all values corresponding to a Autonomous Community.
+    Districte : List
+        List containing all values corresponding to a District.
+    Barri : List
+        List containing all values corresponding to a Neighborhood.
+
+    Returns
+    -------
+    word : str
+        Same parameter string or with territorial information added.
+    """
+
     for vector_name, vector in [("barri", Barri), ("districte", Districte), ("municipi", Municipi), ("comunitat autonoma", ComunitatAutonoma)]:
         for term in vector:
             for w in term.split():
@@ -106,6 +156,27 @@ def fuzzy_search_and_modify(word, stop_words_catalan, Barri, Districte, Municipi
 
 
 def modify_query(q, Barri, Districte, Municipi, ComunitatAutonoma):
+    """
+    Process the query to remove stopwords and write it in plain text.
+
+    Parameters
+    ----------
+    q : str
+        A sequence of strings (query).
+    Barri : List
+        List containing all values corresponding to a Neighborhood.
+    Districte : List
+        List containing all values corresponding to a District.
+    Municipi : List
+        List containing all values corresponding to a Municipality.
+    ComunitatAutonoma : List 
+        List containing all values corresponding to a Autonomous Community.
+
+    Returns
+    -------
+    modified_query : str
+        A sequence of strings with territorial information added and without stopwords.
+    """
 
     stop_words_catalan = set(stopwords.words('catalan'))
     stop_words_catalan.add('l')
@@ -116,84 +187,88 @@ def modify_query(q, Barri, Districte, Municipi, ComunitatAutonoma):
 
 
 def query_collection(collection, query, max_results, dataframe):
+    """
+    Find the top 'max_results' most similar tables given a query.
+
+    Parameters
+    ----------
+    collection : ChromaDB collection
+        Embedding and metadata space.
+    query : str
+        A sequence of strings.
+    max_results : int
+        Maximum number of returned tables.
+    dataframe : DataFrame
+        DtaFrame containing the embeddings of the tables in the DataBase
+
+    Returns
+    -------
+    df : DataFrame
+        DataFrame where each row is a table whose content is similar to a given query, in descending order.
+    """
+
     results = collection.query(query_texts=query, n_results=max_results, include=['distances']) 
     df = pd.DataFrame({
                 'id':results['ids'][0], 
                 'score':results['distances'][0],
                 'content': dataframe[dataframe.ID.isin(results['ids'][0])]['Descripcio'],
                 })
-    
     return df
-
-
-def create_collection(df_descr):
-
-    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-                api_key=os.environ["OPENAI_API_KEY"],
-                model_name="text-embedding-3-small"
-            )
-    
-    client = chromadb.Client()
-    collection = client.get_or_create_collection(name = "descripcions_taules", embedding_function=openai_ef, metadata = {"hnsw:space": "cosine"})
-
-
-    desc = df_descr['Descripcio'].tolist()
-    ids = df_descr['ID'].tolist()
-    collection.add(documents=desc, ids=ids)
-
-    return collection
     
 
-def relevant_docs(q):
+def relevant_docs(q, max_results=10):
+    """
+    Find the top 'max_results' most similar tables given a query.
+
+    Parameters
+    ----------
+    q : str
+        A sequence of strings.
+    max_results : int
+        Maximum number of returned tables.
+
+    Returns
+    -------
+    indicators : List
+        List containing the 'max_results' most similar tables of the DataBase.
+    """
 
     dt = get_data()
-    
     Municipi, AreaMetropolitana, ComunitatAutonoma, Districte, Barri = get_territory_values(dt)
-
-    # Create a new DataFrame with ID and text columns
-    df_descr = pd.DataFrame({
-        'ID': dt['id_indicador'], ##################################################################################################################################################
-        #'nom_taula': dt['indicador.ca'],
-        'Descripcio': dt.apply(generar_text, axis=1)
-    })
+    df = pd.read_csv('./embedded_descr_large_weight.csv', sep = ';')
 
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
                 api_key=os.environ["OPENAI_API_KEY"],
-                model_name="text-embedding-3-small"
+                model_name="text-embedding-3-large"
             )
     
     client = chromadb.Client()
 
-    collection = client.get_or_create_collection(name = "test", embedding_function=openai_ef, metadata = {"hnsw:space": "cosine"})
+    if "new_collection" in [c.name for c in client.list_collections()]:
+        collection = client.get_or_create_collection(name = "new_collection", embedding_function=openai_ef, metadata = {"hnsw:space": "cosine"})
+    else:
+        ids = df['ID'].tolist()
+        df['embd'] = df['embeddings'].apply(ast.literal_eval)
+        list_of_embd = df['embd'].tolist()
+        collection = client.get_or_create_collection(name = "new_collection", embedding_function=openai_ef, metadata = {"hnsw:space": "cosine"})
+        collection.add(ids=ids, embeddings = list_of_embd)
 
-    desc = df_descr['Descripcio'].tolist()
-    ids = df_descr['ID'].tolist()
-    
-    collection.add(documents=desc, ids=ids)
 
     modified_query = modify_query(q, Barri, Districte, Municipi, ComunitatAutonoma)
 
     query_result = query_collection(
-        collection=collection,
-        query=modified_query,
-        max_results=10,
-        dataframe=df_descr
+        collection = collection,
+        query = modified_query,
+        max_results = max_results,
+        dataframe = df
     )
 
-    ruta_archivo_json = "./DataBase/diccionario.json"
+    # Modify the names of the tables to transform its ids into a explanatory sentence
+    json_file = "./diccionario.json"
 
-    # Leer el archivo JSON
-    with open(ruta_archivo_json, "r") as archivo:
-        diccionario_leido = json.load(archivo)
-    for i in ids:
-        if i not in diccionario_leido:
-            print(i)
+    with open(json_file, "r") as file:
+        dictionary_read = json.load(file)
+    indicators = [dictionary_read[i] for i in list(query_result['id'])]
 
-    l_id = list(query_result['id'])
-    #print(l_id)
-    indicadors = []
-    for i in l_id:
-        indicadors.append(diccionario_leido[i])
-
-    return indicadors
+    return indicators
     
