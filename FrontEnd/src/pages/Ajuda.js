664bc4cc8096a82e893aab4f5b97d4e7a8144ec1
JP <jpniznhy@hotmail.com>
2024-05-28 10:45:50 +0200

    ajuda cell adjustment

--- FrontEnd/src/pages/Ajuda.js
+++ FrontEnd/src/pages/Ajuda.js
@@ -18,42 +18,42 @@ function Ajuda() {
       <Logo subtitle="AJUDA"/>
       <div className="grid_container">
         
-        <a href="https://opendata-ajuntament.barcelona.cat/es" className='One' target="_blank">
+        <a href="https://opendata-ajuntament.barcelona.cat/es" className='cell' target="_blank">
           <img src={img_opendata} alt='Open Data BCN'/>
           <div className='text_link'>
             Open Data BCN
           </div>
         </a>
     
-        <a href="https://portaldades.ajuntament.barcelona.cat/ca/" className='Two' target="_blank">
+        <a href="https://portaldades.ajuntament.barcelona.cat/ca/" className='cell' target="_blank">
           <img src={img_bcn_dades} alt='Barcelona Dades'/>
           <div className='text_link'>
             Barcelona Dades
           </div>
         </a>
     
-        <a href="https://www.idescat.cat/?lang=es" className='Three' target="_blank">
+        <a href="https://www.idescat.cat/?lang=es" className='cell' target="_blank">
           <img src={img_idescat} alt="Institut d'Estadística de idescat"/>
           <div className='text_link'>
             Institut d'Estadística de Catalunya
           </div>
         </a>
 
-        <a href="https://ajuntament.barcelona.cat/ca/" className='Four' target="_blank">
+        <a href="https://ajuntament.barcelona.cat/ca/" className='cell' target="_blank">
           <img src={img_aj} alt='Ajuntament de Barcelona'/>
           <div className='text_link'>
             Ajuntament de Barcelona
           </div>
         </a>
     
-        <a href="https://web.gencat.cat/ca/inici/index.html" className='Five' target="_blank">
+        <a href="https://web.gencat.cat/ca/inici/index.html" className='cell' target="_blank">
           <img src={img_generalitat} alt='Generalitat de Catalunya'/>
           <div className='text_link'>
             Generalitat de Catalunya
           </div>
         </a>
     
-        <a href="https://www.ine.es/" className='Six' target="_blank">
+        <a href="https://www.ine.es/" className='cell' target="_blank">
           <img src={img_ine} alt="Institut Nacional d'Estadística"/>
           <div className='text_link'>
             Institut Nacional d'Estadística
