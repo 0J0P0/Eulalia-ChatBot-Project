b813418ac17e7126a2a91a15129a4e6abdab5538
Noa Mediavilla <noamediavilla@hotmail.com>
2024-05-29 03:16:45 +0200

    Correcció textos

--- FrontEnd/src/components/chat/InitialChat.js
+++ FrontEnd/src/components/chat/InitialChat.js
@@ -5,12 +5,12 @@ function InitialChat() {
   return (
     <div className='initial_container'>
       <div className='intro_container'>
-        <h3>Què ès Eulàlia?</h3>
+        <h3>Què és Eulàlia?</h3>
         <p>Eulàlia és un assistent de xat dissenyat per oferir suport intern a l'equip de tècnics de l'Ajuntament de Barcelona.</p>
       </div>
       <div className='intro_container'>
         <h3>Com usar Eulàlia?</h3>
-        <p>Per començar a parlar amb Eulàlia, simplement escriu la teva pregunta en el xat de sota.</p>
+        <p>Per començar a parlar amb Eulàlia, escriu la teva pregunta en el xat de sota.</p>
       </div>
     </div>
   );
